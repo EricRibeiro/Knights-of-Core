07da00ff70fd3a9d17a76eb71e8f0c0ff7b7f956
Eric Ribeiro <ericribeiro@outlook.com.br>
2016-10-30 22:40:19 -0200

    Site's first version files

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function loadXMLDoc() {
+			var xmlhttp = new XMLHttpRequest();
+			var codigo = document.getElementById("cod").value;
+			var departamento = document.getElementById("dep").value;
+			xmlhttp.onreadystatechange=function() {
+				if (xmlhttp.readyState==4) {  //&& xmlhttp.status==200) {               
+					document.getElementById("secao").innerHTML = xmlhttp.responseText;
+				}
+			}
+            xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/departments", true);
+			xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+			var dataToSend = '_data={"dept_no":  "'+ codigo + '", "dept_name":  "' + departamento + '"}';
+
+			xmlhttp.send(dataToSend);
+		}
