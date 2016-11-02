9dea9e2c2f1071f308efb8b3d8a32b229c701447
Eric Ribeiro <ericribeiro@outlook.com.br>
2016-11-02 13:39:53 -0200

    changed the dataToSend parameters, maintainability is improved

--- assets/js/create.js
+++ assets/js/create.js
@@ -1,27 +1,27 @@
 function CriarPersonagem() {
-			var xmlhttp = new XMLHttpRequest();
-			var nome = document.getElementById("nome").value;
-			var classe = document.getElementById("classe").value;
-			var role = document.getElementById("role").value;
-			var especializacao = document.getElementById("especializacao").value;
-			var item_level = document.getElementById("item_level").value;
-			xmlhttp.onreadystatechange=function() {
-				if (xmlhttp.readyState==4) {              
-					document.getElementById("formulario").innerHTML = xmlhttp.responseText;
-				}
-			}
-            xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_personagem/0?_expand=yes", true);
-			xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-			var data = {
-				"nome": nome,
-				"classe": classe,
-				"role": role,
-				"especializacao": especializacao,
-				"item_level": item_level
-			}
-
-			var dataToSend = '_data=' + JSON.stringify(data);
-
-			xmlhttp.send(dataToSend);
+	var xmlhttp = new XMLHttpRequest();
+	var nome = document.getElementById("nome").value;
+	var classe = document.getElementById("classe").value;
+	var role = document.getElementById("role").value;
+	var especializacao = document.getElementById("especializacao").value;
+	var item_level = document.getElementById("item_level").value;
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4) {              
+			document.getElementById("formulario").innerHTML = xmlhttp.responseText;
 		}
+	}
+    xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_personagem/0?_expand=yes", true);
+	xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+	var data = {
+		"nome": nome,
+		"classe": classe,
+		"role": role,
+		"especializacao": especializacao,
+		"item_level": item_level
+	}
+
+	var dataToSend = '_data=' + JSON.stringify(data);
+
+	xmlhttp.send(dataToSend);
+}
