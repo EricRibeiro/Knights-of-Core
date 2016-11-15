7a13e377c6821a9c2a807b74641be374867efdee
Eric Ribeiro <ericribeiro@outlook.com.br>
2016-11-14 23:30:43 -0200

    delete was fixed

--- assets/js/delete.js
+++ assets/js/delete.js
@@ -1,14 +1,16 @@
 var xmlhttp = new XMLHttpRequest();
-var id=0;
-
+var bookId = [];
+var buttonId;
 // Function to delete the character
-function deleteChar() {
+function deleteChar(btnId) {
 	xmlhttp.onreadystatechange=function() {
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {              
 			window.alert("Entrada excluida!");
+			window.location.reload();
+
 		}
 	}
-    xmlhttp.open("DELETE", "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_personagem/" + id, true);
+    xmlhttp.open("DELETE", "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_personagem/" + btnId, true);
 	xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 	xmlhttp.send();
 }
@@ -20,13 +22,17 @@ function processarRequisicao() {
 		var a = JSON.parse (xmlhttp.responseText);
 		var num = a.restify.rows.length;
 		var str = "<table id ='myTable' class='container table table-hover sith'> <thead class='tableHeader'> <th> ID </th> <th> Nome </th> <th> Classe </th> <th> Role </th> <th> Especialização </th> <th> Item level </th> </thead>";
-		for (j = 0; j < num; j++) {
-		id = a.restify.rows[j].values.id.value;
-			str += '<tr>' + '<td>' + id + '</td>' + '<td>' + a.restify.rows[j].values.nome.value + '</td>' + '<td>' + a.restify.rows[j].values.classe.value + '</td>' + '<td>' + a.restify.rows[j].values.role.value + '</td>' + '<td>' + a.restify.rows[j].values.especializacao.value + '</td>' + '<td>' + a.restify.rows[j].values.item_level.value + '</td>' + '<td>' + '<button class="btn btn-primary" onclick="deleteChar()">Delete</button>' + '</td>' + '</tr>';
+		for (i = 0; i < num; i++) {
+			bookId[i] = a.restify.rows[i].values.id.value;
+			str += '<tr>' + '<td>' + a.restify.rows[i].values.id.value + '</td>' + '<td>' + a.restify.rows[i].values.nome.value + '</td>' + '<td>' + a.restify.rows[i].values.classe.value + '</td>' + '<td>' + a.restify.rows[i].values.role.value + '</td>' + '<td>' + a.restify.rows[i].values.especializacao.value + '</td>' + '<td>' + a.restify.rows[i].values.item_level.value + '</td>' + '<td>' + '<button id="button" class="btn btn-primary" onclick="deleteChar(this.id)">Delete</button>' + '</td>' + '</tr>';
 		}
 		document.getElementById("coreTable").innerHTML = str;
 		troca_classe("coreTable","container coolTable jumbotron ");
 		cria_botao();
+		var idLength = bookId.length;
+		for (j = 0; j < idLength; j++) {
+			document.getElementsByTagName("button")[j].id = bookId[j];
+		}
 	}
 }
 
@@ -46,14 +52,3 @@ function cria_botao (){
 function troca_classe (x,y){
 	document.getElementById(x).className = y;
 }
-
-/*//Function to get the cell's value
-  function cellValue() {
-        var table = document.getElementById('myTable');
-        for (var r = 0, n = table.rows.length; r < n; r++) {
-            for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
-                alert(table.rows[r].cells[c].innerHTML);
-            }
-        }
-    }
-*/
